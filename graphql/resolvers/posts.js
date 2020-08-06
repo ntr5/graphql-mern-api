a6cacfb04ff0659d870baa37155a73d9b8feabbb
ntr5 <pressntr@gmail.com>
2020-08-05 23:55:06 -0600

    added fuctionality for likes and comments including subscriptions

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { authenticationError, AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const  checkAuth = require('../../util/check-auth');
@@ -41,6 +41,10 @@ module.exports = {
 
             const post = await newPost.save();
 
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            })
+
             return post;
         },
         async deletePost(_, { postId }, context){
@@ -51,13 +55,39 @@ module.exports = {
                 if(user.username === post.username) {
                     await post.delete();
                     return 'Post deleted successfully';
-
                 } else {
                     throw new AuthenticationError('Action not allowed');
                 }
             } catch(err){
                 throw new Error(err);
             }
+        },
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError('Post not found');
+            }
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 }
